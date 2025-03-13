e90c1ddabf5283da2cf7a59758c63c2ba3777c6f
Bjorn Verbakel <bjorn.verbakel@student.fontys.nl>
2025-03-13 17:36:56 +0100

    github sync with gitlab

--- assets/javascript/javascript.js
+++
@@ -1,7 +0,0 @@
-// Send popup alert on click of contact page
-let contactBtns = document.querySelectorAll('.contactBtn');
-contactBtns.forEach(contactBtn => {
-  contactBtn.addEventListener('click', () => {
-    alert('Contact page is currently unavailable.');
-  });
-});
